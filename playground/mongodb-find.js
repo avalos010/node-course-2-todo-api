28c21e2b882f796b18b01e7cb830ea40e6feab1e
avalos010 <luizavalos40@gmail.com>
2018-02-07 10:53:01 -0600

    init

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -20,9 +20,9 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',
     //     err => console.log('Unable to fetch todos', err));
 
 
-    db.collection('Users').find({name: 'Jluis'}).toArray()
-    .then(docs => console.log(JSON.stringify(docs,undefined,2)),
-    err => console.log('Failed to fetch', err)
-);
+//     db.collection('Users').find({name: 'Jluis'}).toArray()
+//     .then(docs => console.log(JSON.stringify(docs,undefined,2)),
+//     err => console.log('Failed to fetch', err)
+// );
     // database.close();
 });
